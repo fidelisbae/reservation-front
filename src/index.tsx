87d15329f90450dd149b229ea9b2460ee5271f12
Sungrinhan <sungrin92@gmail.com>
2023-08-26 22:13:02 +0900

    Refactor: root 디렉토리 App.tsx, index.tsx 수정
    1. App.tsx 에 Router 컴포넌트 추가
    2. Root index.tsx 에 글로벌 스타일, antd layout 추가

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,19 @@ import App from "./App";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
 
+import AntdLayout from "./components/AntdLayout";
+import GlobalStyle from "./styles/GlobalStyles";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <GlobalStyle />
+      <AntdLayout>
+        <App />
+      </AntdLayout>
     </Provider>
   </React.StrictMode>
 );
